553d98ec66d15245e6c045a215c159b08a3ba1d6
MiltonRamos2002 <milton.ramos954@ist17dejulio.edu.ec>
2023-07-14 09:42:07 -0500

    Versinamiento de hasetst

---
+++ src/main/java/com/mycompany/hastset/HASTSET.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hastset;
+
+/**
+ *
+ * @author MSI-THIN
+ */
+public class HASTSET {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
